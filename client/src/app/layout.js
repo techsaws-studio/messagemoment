1590d6ea2ab89342b288c827dc9e1806d99e0507
Syed Hassan Ali <syeddhassannali@gmail.com>
2025-06-23 08:00:01 +0500

    client: meta depriciation warning

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -15,27 +15,47 @@ export default function RootLayout({ children }) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, viewport-fit=cover"
         />
-        <meta name="description" content="MessageMoment - The real meaning to personal!"/>
-        <meta name="title" content="MessageMoment - The real meaning to personal!"/>
+        <meta
+          name="description"
+          content="MessageMoment - The real meaning to personal!"
+        />
+        <meta
+          name="title"
+          content="MessageMoment - The real meaning to personal!"
+        />
 
         <meta name="application-name" content="MessageMoment" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="MessageMoment" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="MessageMoment - The real meaning to personal!" />
-        <meta property="og:description" content="MessageMoment - The real meaning to personal!" />
-        <meta property="og:url" content="https://message-moment-app.vercel.app"/>
-        <meta property="og:image" content="https://message-moment-app.vercel.app/android-chrome-512x512.png"/>
-        <meta property="og:site_name" content="MessageMoment"/>
-        <meta property="og:image:width" content="1200"/>
-        <meta property="og:image:height" content="630"/>
-        <meta property="og:image:type" content="image/png"/>
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="twitter:title" content={metadata.title}/>
-        <meta name="twitter:description" content={metadata.description}/>
-        <meta name="twitter:image" content="https://message-moment-app.vercel.app/android-chrome-512x512.png"/>
-
+        <meta
+          property="og:title"
+          content="MessageMoment - The real meaning to personal!"
+        />
+        <meta
+          property="og:description"
+          content="MessageMoment - The real meaning to personal!"
+        />
+        <meta
+          property="og:url"
+          content="https://message-moment-app.vercel.app"
+        />
+        <meta
+          property="og:image"
+          content="https://message-moment-app.vercel.app/android-chrome-512x512.png"
+        />
+        <meta property="og:site_name" content="MessageMoment" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:type" content="image/png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta
+          name="twitter:image"
+          content="https://message-moment-app.vercel.app/android-chrome-512x512.png"
+        />
 
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" href="/favicon.png" />
