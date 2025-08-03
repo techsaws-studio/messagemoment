b09355cda4f1eb463d0cdaeb9d84d47132539412
SyeddHassannAli <syeddhassannali@gmail.com>
2025-08-04 02:26:53 +0500

    redirect to chat page locked-session for now (deployment error fixed)

--- client/src/custom-pages/locked-session-page.js
+++ client/src/custom-pages/locked-session-page.js
@@ -1,26 +1,33 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
 import SessionHeader from "@/components/session-header";
 import Session from "@/components/session/session";
 import LockedSessionContent from "@/components/locked-session-content";
+import PageLoader from "@/components/page-loader";
 
 import LockImg from "@/assets/icons/lock.svg";
 
-function LockedSessionPage() {
+function LockedSessionWrapper() {
   const searchParams = useSearchParams();
   const sessionId = searchParams.get("sessionId");
 
   return (
-    <>
-      <SessionHeader />
-      <Session imgName={LockImg}>
-        <LockedSessionContent sessionId={sessionId} />
-      </Session>
-    </>
+    <Session imgName={LockImg}>
+      <LockedSessionContent sessionId={sessionId} />
+    </Session>
   );
 }
 
-export default LockedSessionPage;
+export default function LockedSessionPage() {
+  return (
+    <>
+      <SessionHeader />
+      <Suspense fallback={<PageLoader />}>
+        <LockedSessionWrapper />
+      </Suspense>
+    </>
+  );
+}
