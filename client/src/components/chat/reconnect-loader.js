63e9297343353f35a305e86c1b0d493a3cd013f9
SyeddHassannAli <syeddhassannali@gmail.com>
2025-08-14 18:12:19 +0500

    add reconnect loader

---
+++ client/src/components/chat/reconnect-loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ReconnectLoader = () => {
+  return (
+    <main className="page-loader-container">
+      <div className="page-loader-ui" />
+      Reconnecting
+    </main>
+  );
+};
+
+export default ReconnectLoader;
