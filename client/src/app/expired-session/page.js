54bc759d51c7eba51c4ab22eefc694461112b373
syeddhassannaali <syeddhassannaali@gmail.com>
2025-09-01 13:13:49 +0500

    frontend: expired link container wrapper fix implemented

--- client/src/app/expired-session/page.js
+++ client/src/app/expired-session/page.js
@@ -1,18 +1,13 @@
 import React from "react";
-
 import ExpiredSessionContent from "@/components/expired-session-content";
 import SessionHeader from "@/components/session-header";
-import Session from "@/components/session/session";
 
-import ExpiredsessionImg from "@/assets/icons/expired-session.svg";
 
 function ExpiredSession() {
   return (
     <>
       <SessionHeader />
-      <Session key={`expired-session`} imgName={ExpiredsessionImg}>
-        <ExpiredSessionContent />
-      </Session>
+      <ExpiredSessionContent />
     </>
   );
 }
--- client/src/components/expired-session-content/index.js
+++ client/src/components/expired-session-content/index.js
@@ -2,25 +2,20 @@
 
 import React, { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
-
 import { useClientHydration } from "../../hooks/useClientHydration";
-
 import Button from "../button";
 import { Skeleton } from "../skeleton";
+import Blur from "@/assets/images/blur.png";
+import { getYear } from "date-fns";
+import ExpiredsessionImg from "@/assets/icons/expired-session.svg";
+import Image from "next/image";
 
 const SessionUrlDisplay = () => {
   const searchParams = useSearchParams();
   const sessionId = searchParams.get("sessionId");
 
   return (
-    <p
-      className="small link"
-      // style={{
-      //   wordBreak: "break-all",
-      //   overflowWrap: "break-word",
-      //   whiteSpace: "normal",
-      // }}
-    >
+    <p className="small link link-expired">
       https://messagemoment.com/chat/{sessionId || "unknown"}
     </p>
   );
@@ -39,32 +34,45 @@ const SessionUrlSkeleton = () => (
 
 function ExpiredSessionContent() {
   const isHydrated = useClientHydration();
-
+  const currentYear = getYear(new Date());
   return (
-    <div className="expired-content">
-      <h4>
-        The chat session you were invited to with this link is no longer
-        available
-      </h4>
-
-      {isHydrated ? (
-        <Suspense fallback={<SessionUrlSkeleton />}>
-          <SessionUrlDisplay />
-        </Suspense>
-      ) : (
-        <SessionUrlSkeleton />
-      )}
-
-      <p className="small return-text">
-        Return to the homepage to generate a new chat session.
-      </p>
-
-      <Button
-        text="Return to Homepage"
-        className="btn-primary text-white responsive-button"
-        onClick={() => (window.location.href = "/")}
-      />
-    </div>
+    <>
+      {/* new changes  */}
+      <div className="expired-container">
+        <Image src={Blur} className="blur-img" alt="Blur" />
+        <div className="card-wrapper">
+          <div className="card">
+            <Image
+              src={ExpiredsessionImg}
+              alt="session-icon"
+              className="session-icon"
+            />
+            <h4>
+              The chat session you were invited to with this link is no longer
+              available
+            </h4>
+            {isHydrated ? (
+              <Suspense fallback={<SessionUrlSkeleton />}>
+                <SessionUrlDisplay />
+              </Suspense>
+            ) : (
+              <SessionUrlSkeleton />
+            )}
+            <p className="small return-text">
+              Return to the homepage to generate a new chat session.
+            </p>
+            <Button
+              text="Return to Homepage"
+              className="btn-primary text-white responsive-button"
+              onClick={() => (window.location.href = "/")}
+            />
+          </div>
+        </div>
+        <p className="note text-dark-gray footer-txt">
+          Copyright © {currentYear} MessageMoment. All rights reserved.
+        </p>
+      </div>
+    </>
   );
 }
 
