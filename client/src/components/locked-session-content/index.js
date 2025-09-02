e714e6176b98b6d1d5a46eaa6596cf9601459b34
syeddhassannaali <syeddhassannaali@gmail.com>
2025-09-02 15:29:47 +0500

    frontend: Implemented try again function on locked session  screen

--- client/src/components/locked-session-content/index.js
+++ client/src/components/locked-session-content/index.js
@@ -1,21 +1,48 @@
 import React from "react";
+import Image from "next/image";
+import { getYear } from "date-fns";
 
 import Button from "../button";
 
+import LockImg from "@/assets/icons/lock.svg";
+import Blur from "@/assets/images/blur.png";
+
 function LockedSessionContent({ sessionId }) {
+  const currentYear = getYear(new Date());
+
   return (
-    <div className="locked-content">
-      <h4>The chat session is locked</h4>
-      <p className="small locked-return-text">
-        Unfortunately you are unable to enter the chat at this time.
+    <div className="expired-container">
+      <Image src={Blur} className="blur-img" alt="Blur" />
+      <div className="card-wrapper">
+        <div className="card locked-card">
+          <Image
+            src={LockImg}
+            alt="locked-session-icon"
+            className="locked-sessionIcon"
+          />
+          <h4 className="locked-title">The chat session is locked</h4>
+          <p className="small locked-return-text">
+            Unfortunately you are unable to enter the chat at this time.
+          </p>
+          <div className="locked-btn-flex">
+            <Button
+              text="Try Again"
+              className="btn-primary text-white responsive-button"
+              onClick={() =>
+                (window.location.href = `${process.env.NEXT_PUBLIC_FRONTEND_URL}/chat/${sessionId}`)
+              }
+            />
+            <Button
+              text="Return to Homepage"
+              className="btn-primary-border text-white responsive-button"
+              onClick={() => (window.location.href = "/")}
+            />
+          </div>
+        </div>
+      </div>
+      <p className="note text-dark-gray footer-txt">
+        Copyright © {currentYear} MessageMoment. All rights reserved.
       </p>
-      <Button
-        text="Return to Homepage"
-        className="btn-primary text-white responsive-button"
-        onClick={() =>
-          (window.location.href = `${process.env.NEXT_PUBLIC_FRONTEND_URL}/chat/${sessionId}`)
-        }
-      />
     </div>
   );
 }
--- client/src/custom-pages/locked-session-page.js
+++ client/src/custom-pages/locked-session-page.js
@@ -4,21 +4,14 @@ import React, { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
 import SessionHeader from "@/components/session-header";
-import Session from "@/components/session/session";
 import LockedSessionContent from "@/components/locked-session-content";
 import PageLoader from "@/components/page-loader";
-
-import LockImg from "@/assets/icons/lock.svg";
 
 function LockedSessionWrapper() {
   const searchParams = useSearchParams();
   const sessionId = searchParams.get("sessionId");
 
-  return (
-    <Session imgName={LockImg}>
-      <LockedSessionContent sessionId={sessionId} />
-    </Session>
-  );
+  return <LockedSessionContent sessionId={sessionId} />;
 }
 
 export default function LockedSessionPage() {
